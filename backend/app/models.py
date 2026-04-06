c6094f6fc797e23307e53b573f938535ec06b477
Subhasish Behera <subhasishbehera083@gmail.com>
2026-04-07 01:59:13 +0530

    failure

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,23 +2,23 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from .database import Base
 
 class User(Base):
-    _tablename_ = "users"
+    __tablename__ = "users"   # ✅ ADD THIS LINE
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(100))   # ✅ patient name stored
+    name = Column(String(100))
     email = Column(String(100), unique=True)
     password = Column(String(200))
-    role = Column(String(20))  # patient/admin
+    role = Column(String(20))
 
 class Doctor(Base):
-    _tablename_ = "doctors"
+    __tablename__ = "doctors"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
     specialization = Column(String(100))
 
 class Appointment(Base):
-    _tablename_ = "appointments"
+    __tablename__ = "appointments"
 
     id = Column(Integer, primary_key=True)
     patient_id = Column(Integer, ForeignKey("users.id"))
@@ -26,7 +26,7 @@ class Appointment(Base):
     time = Column(DateTime)
 
 class AuditLog(Base):
-    _tablename_ = "audit_logs"
+    __tablename__ = "audit_logs"
 
     id = Column(Integer, primary_key=True)
     action = Column(String(50))  # CREATE / DELETE
